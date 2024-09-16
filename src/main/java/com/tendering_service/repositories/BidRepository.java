13f41a457d6e401109243c54c5c59cd213bac636
ners1us <66124861+ners1us@users.noreply.github.com>
2024-09-16 11:07:38 +0300

    feat: add Optional wrapper

--- src/main/java/com/tendering_service/repositories/BidRepository.java
+++ src/main/java/com/tendering_service/repositories/BidRepository.java
@@ -5,12 +5,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BidRepository extends JpaRepository<Bid, UUID> {
-    List<Bid> findByTenderId(UUID tenderId);
+    Optional<List<Bid>> findByTenderId(UUID tenderId);
 
-    List<Bid> findByAuthorId(UUID authorId);
+    Optional<List<Bid>> findByAuthorId(UUID authorId);
 }
 
--- src/main/java/com/tendering_service/repositories/TenderRepository.java
+++ src/main/java/com/tendering_service/repositories/TenderRepository.java
@@ -5,9 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface TenderRepository extends JpaRepository<Tender, UUID> {
-    List<Tender> findByCreatorUsername(String username);
+    Optional<List<Tender>> findByCreatorUsername(String username);
 }
