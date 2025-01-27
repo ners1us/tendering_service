688077c0b18e7ad8c5b488bf139fce1afb4070a7
ners1us <66124861+ners1us@users.noreply.github.com>
2025-01-27 18:00:10 +0300

    chore: remove useless dependencies

--- src/main/java/com/tendering_service/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/tendering_service/exceptions/ResourceNotFoundException.java
@@ -1,8 +1,5 @@
 package com.tendering_service.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 public class ResourceNotFoundException extends RuntimeException {
     public ResourceNotFoundException() {}
 
